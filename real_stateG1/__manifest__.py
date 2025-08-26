d765054ffcc829596cc4062e9d5bede5d7553483
RamiroFerreiro <ramiro.x.ferreiro@gmail.com>
2025-08-25 22:37:46 -0300

    Creo archivo gitignore, agrego atributos al manifest, creo carpeta models

--- real_stateG1/__manifest__.py
+++ real_stateG1/__manifest__.py
@@ -1,3 +1,11 @@
 {
-    'name': 'Inmobiliaria'
+    'name': 'Inmobiliaria',
+    'version': '1.0',
+    'author': 'Ramiro',
+    'category': 'Real Estate',
+    'depends': ['base'],
+    'data': [],          # para las vistas
+    'installable': True,  
+    'application': True,  # para que aparezca en Apps
+
 }
